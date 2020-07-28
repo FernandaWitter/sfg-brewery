62a5e608a5cb28db671398391d865abe70dc6404
Fernanda I B Witter <fernanda.witter@yahoo.com>
2020-07-28 10:33:54 -0300

    Initial commit

---
+++ src/test/java/br/com/or/sfg/brewery/sfgbrewery/SfgBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.or.sfg.brewery.sfgbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgBreweryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
